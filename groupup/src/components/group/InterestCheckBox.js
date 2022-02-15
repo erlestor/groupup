a5c4e8b4a86c9970736cf85340d0083856535a61
Haavard Brummenæs <haavabru@stud.ntnu.no>
2022-02-15 14:30:19 +0100

    Added component for the interest tags for creating groups Co-authored by: Kasperrf <kasperrf@stud.ntnu.no>

---
+++ groupup/src/components/group/InterestCheckBox.js
@@ -0,0 +1,26 @@
+// Authored by: Johan solbakken
+import React, { useState } from 'react'
+
+const InterestCheckBox = props => {
+    const [checked, setChecked] = useState(false);
+
+    let title = props.title;
+    let callback = props.callback;
+
+    let backgroundColor = checked ? "green" :  "red";
+
+    return <div 
+        style=
+            {{"background": backgroundColor,
+             "margin": "2px",
+             "padding": "1px",
+             "borderRadius": "4px"}} 
+        onClick={()=>{
+        setChecked(!checked);
+        callback(!checked);
+        }}>
+        {title}
+    </div>
+}
+
+export default InterestCheckBox;
