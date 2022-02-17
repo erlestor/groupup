36161d2a80c531e74f19e3519218e342d189aec7
Haavard Brummenæs <haavabru@stud.ntnu.no>
2022-02-17 15:42:54 +0100

    Added validation and error message on wrong input Co-authored by: kasperrf <kasperrf@stud.ntnu.no>

--- groupup/src/components/groupPage/AddMember.js
+++ groupup/src/components/groupPage/AddMember.js
@@ -1,15 +1,27 @@
 import React, {useState} from 'react'
+import { emailRegex } from '../login/RegisterRegex'
 import "./createGroup.css"
 
 
 const AddMember = () => {
 
-    const [member, setMember] = useState("")
-
+    const [memberEmail, setMemberEmail] = useState("")
+    const [error, setError] = useState("")
     const handleSubmit = (e) => {
         e.preventDefault()
-
+        if (validateForm()) {
+            //POST member to group logic INN HER !!!!
+        }
     }
+
+    const validateForm = () => {
+        if (!memberEmail.match(emailRegex)) {
+            setError("Skriv inn en gyldig mailadresse.")
+            return false
+        }
+        return true
+    }
+
   return (
    <div className='container'>
         <div className = "form-container">
@@ -17,12 +29,14 @@ const AddMember = () => {
                 <label>Legg til gruppemedlem: </label>
                 <input className = "group-input add-member-input"
                 placeholder = "Add member by email"
-                onChange={(e) => setMember(e.target.value)}
+                onChange={(e) => setMemberEmail(e.target.value)}
                 
                 />
                 <button className='btn form-btn' onClick={(e) => handleSubmit(e)}>Legg til medlem i gruppe </button>
+                <p className="error">{error}</p>
             </form>
         </div>
+        
    </div>
   )
 }
