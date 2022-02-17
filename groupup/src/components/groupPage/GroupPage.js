8fe2a4cd4b3392e6d458cbe04860bffeed23f01e
espeasj <espeasj@stud.ntnu.no>
2022-02-17 15:40:18 +0100

    Connect group page to backend
    Co-authored-by: hellekhu <hellekhu@stud.ntnu.no>
    Co-authored-by: Erlend Storsve <erlestor@stud.ntnu.no>

--- groupup/src/components/groupPage/GroupPage.js
+++ groupup/src/components/groupPage/GroupPage.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./group.css"
 import {MdEdit} from "react-icons/md"
+import axios from "../../axios"
 
 const GroupPage = ({id, user}) => {
-
-    //Get group from backend
 
     //Match handle
 
@@ -13,16 +12,68 @@ const GroupPage = ({id, user}) => {
     //Check if member
     const isMember = false
 
-    //Extract info from group
-    const testGroup = ["Vennegjeng fra Gløs", "Hei! Vi er en superdupersuper hyggelig gruppe. Vil dere være med oss på en superduper kul kveld. Kan dere ta kontakt med oss. KAN DERE PLIS TA KONTAKT PLIS PLIS TEKST TESKT ", ["Helle", "Espen"], ["Football","Skiing","Football","Skiing","Football","Skiing","Football","Skiing","Football","Skiing"], "Trondheim", "23.04.22"]
-    const name = testGroup[0]
-    const description = testGroup[1]
-    const members = testGroup[2]
-    const interests = testGroup[3]
-    const location = testGroup[4]
-    const meetingDate = testGroup[5]
-    const membersString = members.join(', ')
-    const interestsString = interests.join(', ')
+    //Age span
+    const thisYear = new Date().getFullYear()
+    const [span, setSpan] = useState("");
+
+    const [name, setName] = useState("")
+    const [description, setDescription] = useState("")
+    const [members, setMembers] = useState([""])
+    const [interests, setInterests] = useState([""])
+    const [location, setLocation] = useState("")
+    const [date, setDate] = useState("")
+    const [usernames, setUsernames] = useState([""])
+
+    //Get group from backend
+    const getGroup = async () => {
+      await axios.get("/groups").then((response) => {
+        const groups = response.data
+        console.log(groups)
+        const matchingGroups = groups.filter(g => g._id === id)
+        if (matchingGroups.length > 0) {
+            const {name, description, members, interests, location, date} = matchingGroups[0]
+            setName(name)
+            setDescription(description)
+            setMembers(members)
+            setInterests(interests)
+            setLocation(location)
+            setDate(date)
+        } else {
+          console.error("no matching group")
+        }
+      }).catch((err) => {
+        console.error(err)
+      })
+    } 
+
+    const calcAgeSpan = async () => {
+      if (members.length > 0) {
+        await axios.get("/users").then((response) => {
+          const users = response.data
+          const usersInMembers = users.filter(user => members.includes(user.email))
+          setUsernames(usersInMembers.map(user => user.name))
+          const userAges = []
+          usersInMembers.forEach(element => {
+            userAges.push(parseInt(element.birthDate.split('-')[0]))
+          });
+          
+          const span =  (thisYear - Math.max(...userAges))+" to "+(thisYear - Math.min(...userAges))
+          setSpan(span)
+        }).catch((err) => {
+          console.error(err)
+        })
+      }
+    }
+
+    useEffect(() => {
+      getGroup()
+    }, [])
+
+    useEffect(() => {
+      calcAgeSpan()
+    }, [members])
+    
+
 
     //HTML 
   return (
@@ -31,10 +82,11 @@ const GroupPage = ({id, user}) => {
       <h2 className='group-name-title'>{name}</h2>
         <img src={require("./friends.png")} />
         <div className='group-main-info'>
-          <div><span className="bold">Members:</span>  {membersString}</div>
-          <div><span className="bold">Interests:</span> {interestsString}</div>
+          <div><span className="bold">Members:</span>  {usernames.join(', ')}</div>
+          <div><span className="bold">Interests:</span> {interests.join(', ')}</div>
           <div><span className="bold">Location:</span>  {location}</div>
-          <div><span className="bold">When to meet:</span>  {meetingDate}</div>
+          <div><span className="bold">Age span:</span>  {span}</div>
+          <div><span className="bold">When to meet:</span>  {date}</div>
         </div>
         {(isMember || isAdmin) ? "" : <button className='match-button'>MATCH</button>}
         <div className='group-desc'>
