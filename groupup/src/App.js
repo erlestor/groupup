cf61ab7c68f3b1e6f1a108996f8321a115974aa1
Erlend Storsve <erlestor@stud.ntnu.no>
2022-02-21 13:28:24 +0100

    added router to app

--- groupup/src/App.js
+++ groupup/src/App.js
@@ -1,18 +1,37 @@
+import { useState } from "react"
 import "./App.css"
 import LoginPage from "./components/login/LoginPage"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import GroupList from "./components/groupPage/GroupList"
 import GroupPage from "./components/groupPage/GroupPage"
+import AddMember from "./components/groupPage/AddMember"
+import CreateGroup from "./components/groupPage/CreateGroup"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
+  const [user, setUser] = useState(null)
+
   return (
-    <div className="app-container">
-      <Header />
-      {/* <LoginPage /> */}
-      <GroupPage />
-      <Footer />
-    </div>
+    <Router>
+      <div className="app-container">
+        <Header user={user} setUser={setUser} />
+        {user ? (
+          <Routes>
+            <Route path="/" element={<GroupList />} />
+            <Route path="/login" element={<LoginPage setUser={setUser} />} />
+            <Route path="/group/:id" element={<GroupPage user={user} />} />
+            <Route path="/group/:id/edit" element={<AddMember user={user} />} />
+            <Route path="/create-group" element={<CreateGroup user={user} />} />
+          </Routes>
+        ) : (
+          <LoginPage setUser={setUser} />
+        )}
+
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
