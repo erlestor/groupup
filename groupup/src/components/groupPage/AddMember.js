385fe9206dd7834ac030b671954462dc27912b0a
Erlend Storsve <erlestor@stud.ntnu.no>
2022-02-21 13:30:12 +0100

    added btn to go back to group page

--- groupup/src/components/groupPage/AddMember.js
+++ groupup/src/components/groupPage/AddMember.js
@@ -1,57 +1,69 @@
-import React, {useState} from 'react'
-import { emailRegex } from '../login/RegisterRegex'
+import React, { useState } from "react"
+import { emailRegex } from "../login/RegisterRegex"
 import "./createGroup.css"
 import axios from "../../axios"
+import { useParams, useNavigate } from "react-router-dom"
 
 const AddMember = () => {
+  // group id
+  const { id } = useParams()
 
-    //Pass groupId to AddMember as a prop
+  const [memberEmail, setMemberEmail] = useState("")
+  const [error, setError] = useState("")
 
-    const groupId = {_id: "620e6400ea0d43a4032d7b09"}
+  const navigate = useNavigate()
 
-    const [memberEmail, setMemberEmail] = useState("")
-    const [error, setError] = useState("")
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if (validateForm()) addMember()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (validateForm()) addMember()
+  }
+
+  const validateForm = () => {
+    if (!memberEmail.match(emailRegex)) {
+      setError("Skriv inn en gyldig mailadresse.")
+      return false
     }
+    return true
+  }
 
-    const validateForm = () => {
-        if (!memberEmail.match(emailRegex)) {
-            setError("Skriv inn en gyldig mailadresse.")
-            return false
-        }
-        return true
-    }
-
-    const addMember = async () => {
-        await axios.put("/addUserToGroup", {
-            userEmail: memberEmail,
-            groupId: groupId
-        }).then((response) => {
-           console.log(response)
-       }).catch((err) => {
-            setError(err.response.data)
-       })
-    }
+  const addMember = async () => {
+    await axios
+      .put("/addUserToGroup", {
+        userEmail: memberEmail,
+        groupId: id,
+      })
+      .then((response) => {
+        console.log(response)
+        setError(`${memberEmail} was added to the group`)
+      })
+      .catch((err) => {
+        setError(err.response.data)
+      })
+  }
 
   return (
-   <div className='container'>
-        <div className = "form-container">
-            <form>
-                <label>Legg til gruppemedlem: </label>
-                <input className = "group-input add-member-input"
-                placeholder = "Add member by email"
-                onChange={(e) => setMemberEmail(e.target.value)}
-                
-                />
-                <button className='btn form-btn' onClick={(e) => handleSubmit(e)}>Legg til medlem i gruppe </button>
-                <p className="error">{error}</p>
-            </form>
-        </div>
-        
-   </div>
+    <div className="container">
+      <div className="form-container">
+        <form>
+          <label>Legg til gruppemedlem: </label>
+          <input
+            className="group-input add-member-input"
+            placeholder="Add member by email"
+            onChange={(e) => setMemberEmail(e.target.value)}
+          />
+          <button className="btn form-btn" onClick={(e) => handleSubmit(e)}>
+            Legg til medlem i gruppe
+          </button>
+          <button
+            className="btn btn-grey"
+            onClick={() => navigate(`/group/${id}`)}
+          >
+            Til gruppesiden
+          </button>
+          <p className="error">{error}</p>
+        </form>
+      </div>
+    </div>
   )
 }
 
