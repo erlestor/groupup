e93dcb06dcb05ff95012897bcdd5a96ca2e372ec
sarajc <sarajc@stud.ntnu.no>
2022-02-22 17:07:09 +0100

    Added filtering for groups Co-authored by: hellekhu <hellekhu@stud.ntnu.no>

--- groupup/src/components/groupPage/GroupList.js
+++ groupup/src/components/groupPage/GroupList.js
@@ -2,32 +2,9 @@ import React, { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import Group from "./Group"
 import "./group.css"
-import axios from "../../axios"
 
-const GroupList = () => {
-  // const groups = [{name: "Venner på gløs", description: "Hei vi er venner på gløs", members: ["haavabru@stud.ntnu.no"], interests:["Ski", "Fotball"], location: "Trondheim", meetingDate: "23.04.22"},{name: "Gode venner på gløs", description: "Hei vi er gode venner på gløs", members: ["haavabru@stud.ntnu.no", "erlendstorsve@gmail.com"], interests:["Ski", "Fotball"], location: "Trondheim", meetingDate: "23.04.22"}]
-  const [groups, setGroups] = useState([])
-  const navigate = useNavigate()
-  //Get group from backend
-  const getGroups = async () => {
-    await axios
-      .get("/groups")
-      .then((response) => {
-        const groups = response.data
-        setGroups(groups)
-      })
-      .catch((err) => {
-        console.error(err)
-      })
-  }
-
-  useEffect(() => {
-    getGroups()
-  }, [])
-
-  const handleClick = () => {
-    navigate("/create-group")
-  }
+const GroupList = ({groups}) => {
+  
 
   const groupMap = groups.map((group, groupIdx) => (
     <Group
@@ -42,11 +19,9 @@ const GroupList = () => {
       key={groupIdx}
     />
   ))
+
   return (
     <div className="group-list">
-      <button onClick={handleClick} className="btn">
-        Create new group
-      </button>
       <h2>All groups</h2>
       {groupMap}
     </div>
