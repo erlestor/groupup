ad343d4cac7503b6c8c2c8c034d22c8c999647f7
Erlend Storsve <erlestor@stud.ntnu.no>
2022-02-28 14:32:11 +0100

    moved group attributes to seperate file

---
+++ groupup/src/components/groupPage/groupAttributes.js
@@ -0,0 +1,25 @@
+export const interests = [
+  "Quiz",
+  "Brettspill",
+  "Trav",
+  "Tur",
+  "Matlaging",
+  "Sport",
+  "Dataspill",
+  "Klatring",
+  "Frisbeegolf",
+]
+
+export const fylker = [
+  "Oslo",
+  "Rogaland",
+  "Møre og Romsdal",
+  "Nordland",
+  "Viken",
+  "Innlandet",
+  "Vestfold og Telemark",
+  "Agder",
+  "Vestland",
+  "Trøndelag",
+  "Troms og Finnmark",
+]
