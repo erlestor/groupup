d5594b2899f70549d59d3a0e5d3106a31766a8bd
Erlend Storsve <erlestor@stud.ntnu.no>
2022-03-01 09:28:11 +0100

    made edit group test

--- groupup/src/tests/EditGroup.spec.js
+++ groupup/src/tests/EditGroup.spec.js
@@ -1,15 +1,52 @@
 import React from "react"
 import { mount } from "@cypress/react"
-import { BrowserRouter as Router } from "react-router-dom"
-// import App from "../App"
-import EditGroup from "../components/groupPage/EditGroup"
+import App from "../App"
 
-describe("Edit group component", () => {
-  it("wrong email/password combination gives error", () => {
-    mount(
-      <Router>
-        <EditGroup />
-      </Router>
-    )
+const login = () => {
+  mount(<App />)
+
+  cy.get("#login-email").type("test@test.test")
+  cy.get("#login-password").type("test")
+  cy.get("#login-submit").click()
+}
+
+describe("Edit Group Component", () => {
+  it("Group details show on load", () => {
+    login()
+
+    cy.contains("test edit group").click()
+    cy.get(".edit-button").click()
+
+    // wait for the first response to finish
+    cy.get("#group-name")
+      .invoke("val")
+      .then((txt) => cy.expect(txt).to.equal("test edit group"))
+    cy.get("#group-description")
+      .invoke("val")
+      .then((txt) => cy.expect(txt).to.equal("testing purposes"))
+    cy.get(".select-fylke")
+      .invoke("val")
+      .then((txt) => cy.expect(txt).to.equal("Trøndelag"))
+  })
+
+  it("editing group details works", () => {
+    login()
+
+    cy.contains("test edit group").click()
+    cy.get(".edit-button").click()
+
+    cy.get("#group-name").clear().type("new group name")
+    cy.get("#group-description").clear().type("new group description")
+    cy.get("#save-edit-group-btn").click()
+    cy.get("#to-group-page-btn").click()
+
+    // sjekke contains her
+    cy.get(".group-name-title").contains("new group name")
+    cy.get(".group-desc").contains("new group description")
+
+    cy.get(".edit-button").click()
+    cy.get("#group-name").clear().type("test edit group")
+    cy.get("#group-description").clear().type("testing purposes")
+    cy.get("#save-edit-group-btn").click()
   })
 })
