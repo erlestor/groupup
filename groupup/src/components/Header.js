78594123529301ccf88d25e260abaa98c6ac1e87
Erlend Storsve <erlestor@stud.ntnu.no>
2022-03-10 13:54:21 +0100

    added links in navbar

--- groupup/src/components/Header.js
+++ groupup/src/components/Header.js
@@ -1,8 +1,9 @@
 import React from "react"
 import { HiUserGroup } from "react-icons/hi"
+import { FaUserCircle } from "react-icons/fa"
 import { useNavigate, Link } from "react-router-dom"
 
-const Header = ({ user, setUser }) => {
+const Header = ({ user, setUser, group }) => {
   const navigate = useNavigate()
 
   const handleButton = () => {
@@ -12,18 +13,41 @@ const Header = ({ user, setUser }) => {
     } else navigate("/login")
   }
 
+  const handleGroupClick = () => {
+    navigate(`/group`)
+  }
+
   return (
     <div className="header">
       <Link to="/" className="router-link">
         <HiUserGroup size={40} />
         <h1 className="header-text">GroupUp</h1>
       </Link>
-      <div className="header-login-container">
-        {user && <p id="user-email">{user.email}</p>}
-        <button className="btn" onClick={handleButton} id="sign-in-out-btn">
-          {user ? "Sign out" : "Sign in"}
-        </button>
-      </div>
+      {user && (
+        <Link to="/" className="router-link">
+          <p className="header-text">Select group</p>
+        </Link>
+      )}
+      {group && (
+        <Link to="/match" className="router-link">
+          <p className="header-text">Find matches</p>
+        </Link>
+      )}
+      {group && (
+        <div className="header-group-container" onClick={handleGroupClick}>
+          <HiUserGroup size={30} />
+          <p>{group.name}</p>
+        </div>
+      )}
+      {user && (
+        <div className="header-group-container">
+          <FaUserCircle size={30} />
+          <p id="user-email">{user.email}</p>
+        </div>
+      )}
+      <button className="btn header-btn" onClick={handleButton}>
+        {user ? "Sign out" : "Sign in"}
+      </button>
     </div>
   )
 }
